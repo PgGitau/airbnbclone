232a80a3eb640e910128f7a941b9123a01781e2f
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-09-28 18:21:12 +0300

    add new logic in card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,13 +2,20 @@ import './Card.css';
 import girl from '../../Images/girl.webp';
 
 function Card(props) {
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card">
             {/* below it should be <img src={`../../images/${props.img}`}
                 instead for it to work now i've replaced with <img src={girl}
             */}
-            {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
-            {/* ABOVE: If the left part resolves as truthy the right part will be run and viceversa*/}
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            {/* ABOVE: If badgeText is truthy(if the text will be rendered) the right part will be run and viceversa*/}
             <img src={girl} className="card--image" alt="user"/>
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" alt="a star"/>
